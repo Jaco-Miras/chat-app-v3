6e15ab96df34da89950ea3f7fde2d862d13e21d7
Jaco-Miras <miras.jaco@gmail.com>
2025-03-27 06:49:08 +0800

    installing cors and fix the cors error

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
@@ -12,6 +13,12 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: process.env.ORIGIN_URL,
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
