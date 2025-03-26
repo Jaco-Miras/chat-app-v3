ba05b0d896d7d55aae24d3993e0c80ac3961e86a
Jaco-Miras <miras.jaco@gmail.com>
2025-03-27 00:07:36 +0800

    hotfix typo error

--- backend/src/models/message.model.js
+++ backend/src/models/message.model.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const messaegeSchema = new mongoose.Schema(
+const messageSchema = new mongoose.Schema(
   {
     senderId: {
       type: mongoose.Schema.Types.ObjectId,
@@ -22,6 +22,6 @@ const messaegeSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-const Message = mongoose.model("Message", userSchema);
+const Message = mongoose.model("Message", messageSchema);
 
 export default Message;
