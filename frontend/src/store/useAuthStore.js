698dd2430e2be498f5a65a97b29e344d9d70a93f
Jaco-Miras <miras.jaco@gmail.com>
2025-03-27 14:09:24 +0800

    integrate update profile from zustand

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -6,7 +6,7 @@ export const useAuthStore = create((set) => ({
   authUser: null,
   isSigningUp: false,
   isLoggingIn: false,
-  isUpdateingProfile: false,
+  isUpdatingProfile: false,
 
   isCheckingAuth: true,
 
@@ -60,6 +60,20 @@ export const useAuthStore = create((set) => ({
       toast.success("Logged out successfully");
     } catch (error) {
       toast.error(error.response.data.message);
+    }
+  },
+
+  updateProfile: async (data) => {
+    set({ isUpdatingProfile: true });
+    try {
+      const res = await axiosInstance.put("/auth/update-profile", data);
+      set({ authUser: res.data });
+      toast.success("Profile updated successfully");
+    } catch (error) {
+      console.log("error in update profile:", error);
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isUpdateingProfile: false });
     }
   },
 }));
