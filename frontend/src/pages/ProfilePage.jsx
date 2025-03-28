567b0d79efe06213fbac73ccb756c4e3be762a8e
Jaco-Miras <miras.jaco@gmail.com>
2025-03-28 09:46:49 +0800

    update style

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -22,13 +22,15 @@ export const ProfilePage = () => {
   };
 
   return (
-    <div className="h-screen pt-20">
+    <div className="h-full pt-20">
       <div className="max-w-2xl mx-auto p-4 py-8">
         <div className="bg-base-300 rounded-xl p-6 space-y-8">
           <div className="text-center">
             <h1 className="text-2xl font-semibold ">Profile</h1>
             <p className="mt-2">Your profile information</p>
           </div>
+
+          {/* avatar upload section */}
 
           <div className="flex flex-col items-center gap-4">
             <div className="relative">
@@ -40,14 +42,12 @@ export const ProfilePage = () => {
               <label
                 htmlFor="avatar-upload"
                 className={`
-                  absolute bottom-0 right-0 
-                  bg-base-content hover:scale-105
-                  p-2 rounded-full cursor-pointer 
-                  transition-all duration-200
-                  ${
-                    isUpdatingProfile ? "animate-pulse pointer-events-none" : ""
-                  }
-                `}
+                absolute bottom-0 right-0 
+                bg-base-content hover:scale-105
+                p-2 rounded-full cursor-pointer 
+                transition-all duration-200
+                ${isUpdatingProfile ? "animate-pulse pointer-events-none" : ""}
+              `}
               >
                 <Camera className="w-5 h-5 text-base-200" />
                 <input
